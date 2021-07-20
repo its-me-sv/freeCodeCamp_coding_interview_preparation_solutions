81121692b17e3094536fb28f92e28b3dcaae747a
Suraj Vijay <surajcpp115@gmail.com>
2021-07-20 13:05:59 +0530

    made the heap code simple and clear

--- Data_Structures/39_insert_element_into_max_heap.js
+++ Data_Structures/39_insert_element_into_max_heap.js
@@ -1,12 +1,10 @@
 const MaxHeap = function () {
-    this.heap = [null];
-    this.size = 0;
-    this.print = () => this.heap.slice(0, this.size);
+    this.heap = [];
+    this.print = () => this.heap;
     this.insert = data => {
-        this.size += 1;
-        let i;
-        for (i = this.size; i != 0 && this.heap[Math.floor(i / 2)] < data; i = Math.floor(i / 2))
-            this.heap[i] = this.heap[Math.floor(i / 2)];
-        this.heap[i] = data;
+        this.heap.push(data);
+        let size = this.heap.length - 1;
+        for (let i = size; i >= 0 && this.heap[i] > this.heap[Math.floor(i / 2)]; i = Math.floor(i / 2))
+            [this.heap[i], this.heap[Math.floor(i / 2)]] = [this.heap[Math.floor(i / 2)], this.heap[i]];
     };
 };
